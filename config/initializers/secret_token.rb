b512795b6bebaea494564402da5331b84abea391
kelthenoble <keltheceo@gmail.com>
2014-07-14 14:35:10 -0700

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsReflux::Application.config.secret_key_base = '27700bc99f67efb063f78f82f1d84047c2443b8dbd2d15f44b532b9d83e739a3037f2ea0c931e05655dfadd1dd9a35547fe195db500c9f337f0357391091546e'
